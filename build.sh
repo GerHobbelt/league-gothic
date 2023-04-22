4ea6971d29c4f88855d1e46f167667dc70a5eb90
Ger Hobbelt <ger@hobbelt.com>
2023-04-22 14:09:12 +0200

    - udated build shell script - regenerated font files

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@
 #
 # we assume you have https://github.com/googlefonts/fontmake installed properly!
 
-rm -rf master_otf/
-rm -rf master_ttf/
-rm -rf master_ufo/
+rm -rf master_*/
+rm -rf instance_*/
+rm -rf variable_*/
 
 for f in $( find -name *.glyph* ) ; do 
-	fontmake $f 
+	fontmake $f -i
 done
 
-rm -rf master_ufo/
+rm -rf *_ufo/
 
